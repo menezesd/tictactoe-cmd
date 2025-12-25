3afcd7c4573ddbf9434717f787a659511bcb07d7
Dean Menezes <dean.menezes@utexas.edu>
2025-12-25 13:51:43 -0600

    feat: Enhance CLI user interface

--- ttt_cli.c
+++ ttt_cli.c
@@ -6,20 +6,20 @@
 #include <stdbool.h>
 
 
-
 static void show_board(Board b){
     uint16_t x = ttt_bits_x(b), o = ttt_bits_o(b);
+    printf("    a   b   c\n"); // Column coordinates
+    puts("  +---+---+---+");
     for (int r = 0; r < 3; ++r) {
-        puts("+---+""---+---+);
-        printf("|\n");
+        printf("%d |", r + 1); // Row coordinate
         for (int c = 0; c < 3; ++c) {
             int i = r*3 + c;
             char ch = (x & (1u<<i)) ? 'X' : (o & (1u<<i)) ? 'O' : ' ';
             printf(" %c |", ch);
         }
-        puts("");
+        puts(""); // Newline after each row
+        puts("  +---+---+---+"); // Row separator
     }
-    puts("+---+""---+---+);
 }
 
 
@@ -36,7 +36,7 @@ static void usage(const char *prog){
 
 static int get_human_move(Board b) {
     while (true) {
-        printf("Player %c, your move (0-8 or a1..c3): ", token(ttt_side_to_move(b)));
+        printf("\nPlayer %c, your move (0-8 or a1..c3): ", token(ttt_side_to_move(b)));
         fflush(stdout);
         int mv = read_move();
         if (mv == TTT_PARSE_EOF) { // EOF or read error
@@ -83,30 +83,36 @@ static int run_game(ttt_side ai_player) {
     Board b = ttt_initial();
     bool ai_active = (ai_player != (ttt_side)2); 
 
+    printf("Welcome to Tic-Tac-Toe!\n\n");
+
     while (true) {
+        show_board(b);
+
         ttt_score s;
         if (ttt_is_terminal(b, &s)) {
-            show_board(b);
-            if (s == TTT_DRAW) puts("Draw");
-            else printf("%c wins\n", token((ttt_side)((ttt_side_to_move(b)^1))));
+            printf("\n--- GAME OVER ---\n");
+            if (s == TTT_DRAW) {
+                printf("It's a draw!\n");
+            } else {
+                printf("Player %c wins!\n", token((ttt_side)((ttt_side_to_move(b)^1))));
+            }
+            printf("-----------------\n");
             break;
         }
 
         int mv = -1;
         if (ai_active && ttt_side_to_move(b) == ai_player) {
+            printf("\nAI is playing...\n");
             mv = ttt_best_move(b);
+            printf("AI played on square %d\n", mv);
         } else {
-            show_board(b);
-            printf("Player %c, your move (0-8 or a1..c3): ", token(ttt_side_to_move(b)));
-            mv = read_move();
-            if (mv == -1) { /* EOF or read error */ break; } // This will be TTT_PARSE_EOF from previous change
-            if (mv == -1) { fprintf(stderr, "Invalid format. Enter 0-8 or a1-c3.\n"); continue; }
-            if (mv == -2) { fprintf(stderr, "Move out of range. Enter 0-8 or a1-c3.\n"); continue; }
-            if (!ttt_is_legal(b, mv)) {
-                fprintf(stderr, "Illegal move (square occupied or invalid).\n");
-                continue;
+            mv = get_human_move(b);
+            if (mv == -1) { // EOF or read error
+                printf("\nExiting game.\n");
+                break;
             }
         }
+        
         b = ttt_apply(b, mv);
     }
     return 0;
